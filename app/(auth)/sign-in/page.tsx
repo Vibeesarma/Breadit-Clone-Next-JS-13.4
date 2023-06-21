fe7743f886dc06acb003b19dd7c633b140e79693
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-06-21 08:09:34 +0100

    fix: sign in center

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 const SignInPage = () => {
   return (
     <div className="absolute inset-0">
-      <div className="h-full max-w-2xl flex flex-col items-center justify-center gap-20">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
         <Link
           href="/"
           className={cn(
